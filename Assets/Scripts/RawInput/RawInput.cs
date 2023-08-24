de863069ca2f072c0a470f56b47a819e887a2611
AlyxerHL <alyxer@alyxer.me>
2023-08-24 12:42:19 +0900

    Add KeyViewerUI

--- Assets/Scripts/RawInput/RawInput.cs
+++ Assets/Scripts/RawInput/RawInput.cs
@@ -5,8 +5,12 @@ public static class RawInput
     [DllImport("user32.dll")]
     private static extern short GetAsyncKeyState(int keyCode);
 
-    private static bool KeyDown(RawKeyCode keyCode)
+    public static bool KeyDown(RawKeyCode keyCode)
     {
+#if UNITY_EDITOR
+        return UnityEngine.Input.GetKey(UnityEngine.KeyCode.Space);
+#else
         return (GetAsyncKeyState((int)keyCode) & 0x8000) != 0;
+#endif
     }
 }
--- Assets/Scripts/UI/KeyViewerUI.cs
+++ Assets/Scripts/UI/KeyViewerUI.cs
@@ -1,3 +1,22 @@
 using UnityEngine;
 
-public class KeyViewerUI : MonoBehaviour { }
+public class KeyViewerUI : MonoBehaviour
+{
+    [SerializeField]
+    private Key[] keys;
+
+    private void Update()
+    {
+        foreach (var key in keys)
+        {
+            if (RawInput.KeyDown(key.KeyCode))
+            {
+                key.OnPressed();
+            }
+            else
+            {
+                key.OnReleased();
+            }
+        }
+    }
+}

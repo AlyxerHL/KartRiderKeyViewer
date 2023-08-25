c8e9628142765bee37f9bf7ae1ea915b2d385718
AlyxerHL <alyxer@alyxer.me>
2023-08-25 13:24:57 +0900

    Implement ColorInput

--- Assets/Scripts/UI/Elements/ColorInput.cs
+++ Assets/Scripts/UI/Elements/ColorInput.cs
@@ -1,5 +1,43 @@
+using System.Text.RegularExpressions;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
-public class ColorInput : MonoBehaviour { 
-    
+public class ColorInput : MonoBehaviour
+{
+    [Header("Targets")]
+    [SerializeField]
+    private Image preview;
+
+    [SerializeField]
+    private TMP_InputField hex;
+
+    [Header("Properties")]
+    [SerializeField]
+    private Color color;
+
+    [SerializeField]
+    private UnityEvent<Color> onColorChanged;
+
+    public void HandleValueChanged()
+    {
+        hex.text = Regex.Replace(hex.text, "[^0-9a-fA-F]", "").ToUpper();
+    }
+
+    public void HandleEndEdit()
+    {
+        if (ColorUtility.TryParseHtmlString($"#{hex.text}", out color))
+        {
+            preview.color = color;
+        }
+
+        hex.text = ColorUtility.ToHtmlStringRGB(preview.color);
+    }
+
+    private void Awake()
+    {
+        preview.color = color;
+        hex.text = ColorUtility.ToHtmlStringRGB(color);
+    }
 }
